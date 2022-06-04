c1e53130edbaf70a60d9b11b2136690153e161a4
Josileia <josileia.rodrigues@viacometa.com.br>
2022-06-04 10:55:38 -0400

    atualizações finais

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,114 @@
+   
 <template>
-<v-app>
-  <v-container>
-    <v-card>
+  <v-app>
+    <v-container>
       <v-container>
-        <v-text-field 
-        v-model="search"
-        label="Pesquisar"
-        placeholder="Chamander"
-        solo>
-        </v-text-field>
-  
         <v-row>
-          <v-col cols="2" v-for="pokemon in filtered_pokemons " :key="pokemon.name">
-            <v-card>
-              <v-container>
-              <v-row class="mx-0 d-flex justify-center">
-              {{get_id(pokemon)}}
-          <img :src= " `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png `"
-              
-              :alt="pokemon.name"
-              width=100%/>
-              </v-row>
-              <h2 class = "text-center">{{get_name(pokemon)}}</h2>
-              </v-container>
-            </v-card>
+          <v-container>
+           <v-img
+              :src="require('../src/assets/pokemon.png')"
+              class="my-3"
+              contain
+              height="200"
+            />
+            <h1 class="text-center white--text mb-2" style="font-size: 5rem">
+              Pokedex
+            </h1>
+          </v-container>
+        </v-row>
+        <v-text-field
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Charmander"
+          solo
+        ></v-text-field>
+
+        <v-row>
+          <v-col
+            cols="6"
+            md="2"
+            v-for="pokemon in filtered_pokemons"
+            :key="pokemon.name"
+          >
+            <PokemonCard :pokemon="pokemon" @clicked="show_pokemon" />
           </v-col>
         </v-row>
       </v-container>
-    </v-card>
-  </v-container>
-</v-app>
+    </v-container>
+
+  </v-app>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import PokemonCard from "./components/PokemonCard.vue";
+
+
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-
+     PokemonCard,
+  
+ 
+   
   },
-
   data() {
     return {
       pokemons: [],
-      search:"",
-    }
-
-  },
-  computed:{
-    filtered_pokemons(){
-      return this.pokemons.filter((item)=>{
-        return item.name.includes(this.search);
-      })
-
-    }
-  },
-  methods:{
-    get_id(pokemon){
-      return Number(pokemon.url.split("/")[6])
-    },
-    get_name(pokemon){
-      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-
-    }
+      search: "",
+      show_dialog: false,
+      selected_pokemon: null,
+    };
   },
   mounted() {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then((response) => {
-      this.pokemons = response.data.results;
-    })
-  }
-
+   axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=493")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  methods: {
+     show_pokemon(id) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
+    },
+    get_move_level(move) {
+      for (let version of move.version_group_details) {
+        if (
+          version.version_group.name == "sword-shield" &&
+          version.move_learn_method.name == "level-up"
+        ) {
+          return version.level_learned_at;
+        }
+      }
+      return 0;
+    },
+  },
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      });
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-  backgroud: linear-gradient(to bottom right, rgba(10, 10, 10, 1),
-      rgba(12, 39, 63, 1),
-    ) no-repeat center center fixed !important;
-  -webkit-backgroud-size: cover;
-  -o-backgroud: cover !important;
-  backgroud-position: center;
-  main-heigth: 100vh;
-
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
 }
 </style>
